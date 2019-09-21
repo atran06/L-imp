25c9311598dc6404febce619db6dfd259df35a27
Arthur Tran <tran.art13@gmail.com>
2019-09-21 01:01:50 -0700

    Added token recognization for ':='

--- Lexer/lexer.py
+++ Lexer/lexer.py
@@ -6,7 +6,7 @@ Class: Comp141 - Programming Lanuages
 File Description: Basic lexer/scanner that scans for PUNCTUATION, IDENTIFIERs, and NUMBERs.
                   RegEx:    IDENIFIER=([a-z] | [A-Z])([a-z] | [A-Z] | [0-9])*
                             NUMBER=[0-9]+
-                            PUNCTUATION=\+ | \- | \* | / | \( | \)
+                            PUNCTUATION=\+ | \- | \* | / | \( | \) | \; | \:\=
                   Instructions on how to use the program are contained in 'instructions.txt'.
 '''
 
@@ -23,16 +23,31 @@ def scan(line):
 
     # extracting basic tokens between whitespace and punctuation
     token = ""
-    for char in line: # goes through each character in the string and adds the char into the variable 'token' until one of the conditions are met
+    for i, char in enumerate(line): # goes through each character in the string and adds the char into the variable 'token' until one of the conditions are met
         if char == " ":
             if token != "": 
                 basicTokens.append(token) # appends the substring from last append to whitespace
                 token = "" 
-        elif re.search("\+|\-|\*|/|\(|\)|\;", char): # uses regular expressions to check for punctuation
-            if token != "": 
-                basicTokens.append(token) # appends the substring from last append to punctuation
+
+        elif re.search("\+|\-|\*|/|\(|\)|\;|\:|\=", char): # uses regular expressions to check for punctuation
+            if char == ":":
+                if token != "":
+                    basicTokens.append(token)
+                    token = ""
+                token += char
+
+            elif char == "=":
+                token += char
+                basicTokens.append(token)
                 token = ""
-            basicTokens.append(char) # appends the punctuation
+
+            else:
+                if token != "":
+                    basicTokens.append(token)
+                    token = ""
+
+                basicTokens.append(char)
+
         else: token += char
 
     if token != "": basicTokens.append(token) # adds the last token into the list
@@ -62,7 +77,7 @@ def scan(line):
 # Parameters: token - token to be identified
 # Returns: the token type coresponding to the given token
 def identifyTokens(token):
-    if re.search("\+|\-|\*|/|\(|\)|\;", token): return "PUNCTUATION"
+    if re.search("\+|\-|\*|/|\(|\)|\;|\:\=", token): return "PUNCTUATION"
     elif token[0].isdigit(): return "NUMBER"
     elif token[0].isalpha(): return "IDENTIFIER"
     else: return "ERROR"
