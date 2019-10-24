f0dbf9b02799fb2b0c8261dff3a05dd8dbef419e
Arthur Tran <tran.art13@gmail.com>
2019-10-23 22:59:39 -0700

    Removed main function from lexer

--- lexer.py
+++ lexer.py
@@ -85,23 +85,3 @@ def identifyTokens(token):
     elif token[0].isdigit(): return "NUMBER"
     elif token[0].isalpha(): return "IDENTIFIER"
     else: return "ERROR"
-
-def main():
-    # opens the input file specified when running script from terminal
-    inputFile = open(sys.argv[1]) # can change this line to inputFile = open("FILENAME", "r")
-
-    for line in inputFile:
-        print("Line: %s" % line.strip())
-
-        tokens = scan(line.strip())
-        for token in tokens:
-            tokenName = identifyTokens(token) # holds the type of token
-            if tokenName == "ERROR": 
-                print("ERROR READING: \"" + token + "\"")
-                break
-            else: print(token + " : " + tokenName)
-
-        print("")
-
-if __name__ == "__main__":
-    main()
