638c2dfb8ddb93a2218a976e88fd9146259d79de
Arthur Tran <tran.art13@gmail.com>
2019-10-28 19:37:51 -0700

    Documentation

--- tree.py
+++ tree.py
@@ -1,6 +1,10 @@
+'''
+File Description: Quick helper class I made that implements a basic binary tree.
+'''
+
 class Tree:
     def __init__(self, data, tokenType, left, right):
-        self.data = data
-        self.tokenType = tokenType
-        self.left = left
-        self.right = right
+        self.data = data # holds the value of the node
+        self.tokenType = tokenType # holds the type of the value
+        self.left = left # left subtree/node
+        self.right = right # right subree/node
