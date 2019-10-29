4a7f7e74456668513cc6eab6bfb5dbfce3dee87a
Arthur Tran <tran.art13@gmail.com>
2019-10-28 19:19:42 -0700

    Fully implemented parser to follow cfg rules in 'parserE.py'

--- parserE.py
+++ parserE.py
@@ -6,14 +6,65 @@ def parseNumber(tokensAndTypes):
     token = tokensAndTypes[0]
     tokensAndTypes.pop(0)
 
-    return Tree(token[0],token[1], None, None)
+    return Tree(token[0], token[1], None, None)
+
+def parseIdentifier(tokensAndTypes):
+    if tokensAndTypes[0][1] != "IDENTIFIER": return
+
+    token = tokensAndTypes[0]
+    tokensAndTypes.pop(0)
+
+    return Tree(token[0], token[1], None, None)
+
+def parseElement(tokensAndTypes):
+    tree = None
+
+    if tokensAndTypes[0][0] == "(":
+        tokensAndTypes.pop(0)
+
+        tree = parseExpression(tokensAndTypes)
+
+        if tokensAndTypes[0][0] != ")": return
+        tokensAndTypes.pop(0)
+
+    elif tokensAndTypes[0][1] == "NUMBER": tree = parseNumber(tokensAndTypes)
+    else: tree = parseIdentifier(tokensAndTypes)
+
+    return tree
+
+def parsePiece(tokensAndTypes):
+    tree = parseElement(tokensAndTypes)
+
+    while len(tokensAndTypes) > 0 and tokensAndTypes[0][0] == "*":
+        tokensAndTypes.pop(0)
+        tree = Tree("*", "PUNCTUATION", tree, parseElement(tokensAndTypes))
+
+    return tree
+
+def parseFactor(tokensAndTypes):
+    tree = parsePiece(tokensAndTypes)
+
+    while len(tokensAndTypes) > 0 and tokensAndTypes[0][0] == "/":
+        tokensAndTypes.pop(0)
+        tree = Tree("/", "PUNCTUATION", tree, parsePiece(tokensAndTypes))
+
+    return tree
+
+def parseTerm(tokensAndTypes):
+    tree = parseFactor(tokensAndTypes)
+
+    while len(tokensAndTypes) > 0 and tokensAndTypes[0][0] == "-":
+        tokensAndTypes.pop(0)
+        tree = Tree("-", "PUNCTUATION", tree, parseFactor(tokensAndTypes))
+
+    return tree
 
 def parseExpression(tokensAndTypes):
-    tree = parseNumber(tokensAndTypes)
+    tree = parseTerm(tokensAndTypes)
 
     while len(tokensAndTypes) > 0 and tokensAndTypes[0][0] == "+":
         tokensAndTypes.pop(0)
-        tree = Tree("+", "PUNCTUATION", tree, parseNumber(tokensAndTypes))
+        tree = Tree("+", "PUNCTUATION", tree, parseTerm(tokensAndTypes))
 
     return tree
 
